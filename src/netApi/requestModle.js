43b4f74da6a4414caa9695a77e7b38d84a51d21d
max_wang <405323395@qq.com>
2017-10-30 16:18:08 +0800

    项目进度

--- src/netApi/requestModle.js
+++ src/netApi/requestModle.js
@@ -1,102 +1,104 @@
 import Header from './Header.js'
 import router from '../router'
 export default {
-request (url, data, requestSuccess, requestFail, requestComplete, interceptors) {
-  let header = new Header('application/json').getHeader();
+  request(url, data, requestSuccess, requestFail, requestComplete, interceptors) {
+    let header = new Header('application/json').getHeader();
 
-  for (let interceptor of interceptors) {
-    interceptor.onRequest(url, header, data);
-  }
+    for (let interceptor of interceptors) {
+      interceptor.onRequest(url, header, data);
+    }
 
-  data = { formData: JSON.stringify(data) };
+    data = {
+      formData: JSON.stringify(data)
+    };
 
-  //---------------------------------------------------------------
-  let xmlhttp;
-	if (window.XMLHttpRequest)
-	{
-		//  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-		xmlhttp=new XMLHttpRequest();
-	}
-	else
-	{
-		// IE6, IE5 浏览器执行代码
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
+    //---------------------------------------------------------------
+    let xmlhttp;
+    if (window.XMLHttpRequest) {
+      //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      // IE6, IE5 浏览器执行代码
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
 
-	xmlhttp.onreadystatechange=function()
-	{
-    // console.log('readyState = ', xmlhttp.readyState);
-    // console.log('status = ' , xmlhttp.status);
+    xmlhttp.onreadystatechange = function() {
+      // console.log('readyState = ', xmlhttp.readyState);
+      // console.log('status = ' , xmlhttp.status);
 
-		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		{
-      let responseData;
-      try {
-        responseData = JSON.parse(xmlhttp.responseText);
-      } catch (e) {
-        for (let interceptor of interceptors) {
-          interceptor.onServiceError(url, header, responseData);
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        let responseData;
+        try {
+          responseData = JSON.parse(xmlhttp.responseText);
+        } catch (e) {
+          for (let interceptor of interceptors) {
+            interceptor.onServiceError(url, header, responseData);
+          }
+          requestFail('服务器错误的消息格式');
+          return;
         }
-        requestFail('服务器错误的消息格式');
-        return;
+
+        switch (responseData.retCode) {
+          case '0':
+            for (let interceptor of interceptors) {
+              interceptor.onResponse(url, header, responseData);
+            }
+            requestSuccess(responseData.retData);
+            break;
+          case '102':
+            for (let interceptor of interceptors) {
+              interceptor.onAutherErrorResponse(url, header, responseData);
+            }
+            requestFail('登录身份验证不通过');
+            router.replace({
+              path: 'login'
+            })
+            break;
+          case '101':
+            for (let interceptor of interceptors) {
+              interceptor.onFaildResponse(url, header, responseData);
+            }
+            requestFail(responseData.retMsg);
+            break;
+          default:
+            for (let interceptor of interceptors) {
+              interceptor.onFaildResponse(url, header, responseData);
+            }
+            requestFail(responseData.retMsg);
+        }
+        if (requestComplete != undefined) {
+          requestComplete(xmlhttp.responseText);
+        }
+
+      } else if (xmlhttp.readyState == 4) {
+        for (let interceptor of interceptors) {
+          interceptor.onFaildResponse(url, header, {
+            retMsg: "网络请求失败"
+          });
+        }
+        requestFail("网络请求失败");
       }
 
-      switch (responseData.code) {
-        case '0':
-          for (let interceptor of interceptors) {
-            interceptor.onResponse(url, header, responseData);
-          }
-          requestSuccess(responseData.retData);
-          break;
-        case '102':
-          for (let interceptor of interceptors) {
-            interceptor.onAutherErrorResponse(url, header, responseData);
-          }
-          requestFail('登录身份验证不通过');
-          router.replace({ path: 'login' })
-          break;
-        case '101':
-          for (let interceptor of interceptors) {
-            interceptor.onFaildResponse(url, header, responseData);
-          }
-          requestFail(responseData.retMsg);
-          break;
-        default :
-          for (let interceptor of interceptors) {
-            interceptor.onFaildResponse(url, header, responseData);
-          }
-          requestFail(responseData.retMsg);
-      }
-      if (requestComplete != undefined) {
-        requestComplete(xmlhttp.responseText);
-      }
 
-		} else if (xmlhttp.readyState==4) {
-      for (let interceptor of interceptors) {
-        interceptor.onFaildResponse(url, header, { retMsg:"网络请求失败"});
-      }
-      requestFail("网络请求失败");
     }
 
-
-	}
-
-  xmlhttp.open("POST",url,true);
-  for (var key of Object.keys(header)) {
-    xmlhttp.setRequestHeader(key, header[key]);
-  }
-  xmlhttp.withCredentials = true;
-  let param = JSON.stringify(data);
-	xmlhttp.send(param);
-
-  setTimeout(connectFail, 10000);
-  function connectFail() {
-    if (xmlhttp) {
-      xmlhttp.abort();
+    xmlhttp.open("POST", url, true);
+    for (var key of Object.keys(header)) {
+      xmlhttp.setRequestHeader(key, header[key]);
     }
-  }
+    xmlhttp.withCredentials = true;
+    let param = JSON.stringify(data);
+    xmlhttp.send(param);
 
-}
+    setTimeout(connectFail, 10000);
+
+    function connectFail() {
+      if (xmlhttp) {
+        xmlhttp.abort();
+      }
+    }
+
+  }
 
 
 }
