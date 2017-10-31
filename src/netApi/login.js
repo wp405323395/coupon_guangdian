68f946fb5f136ce5e14e3ad67a3ca7d04459c874
max_wang <405323395@qq.com>
2017-10-31 14:59:20 +0800

    项目进度

--- src/netApi/login.js
+++ src/netApi/login.js
@@ -2,13 +2,13 @@
  import RequestEngine from './RequestEngine'
 
  function login(loginCallBack) {
-   //let href = window.location.href;
-   let href =
-     "https://www.maywidehb.com/webapp/index.html?code=081UhLaK1Ylsz70aUt9K1yvEaK1UhLas&state=123";
+   let href = window.location.href;
+   //  let href =
+   //    "https://www.maywidehb.com/webapp/index.html?code=081UhLaK1Ylsz70aUt9K1yvEaK1UhLas&state=123";
    let param = href.split("?")[1];
    let wxcodemap = param.split("&")[0];
    let wxcode = wxcodemap.split("=")[1];
-   console.log(urls.loginUrl);
+
    new RequestEngine().request(urls.loginUrl, {
        loginCode: wxcode
      },
