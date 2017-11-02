6562ac129a3672c287465096783f92305bb42549
max_wang <405323395@qq.com>
2017-11-02 13:04:34 +0800

    项目进度

--- src/netApi/login.js
+++ src/netApi/login.js
@@ -4,14 +4,15 @@
  function login(loginCallBack) {
    let href = window.location.href;
    console.log(href);
-   if (href.indexOf('code=') >= 0) {
-
-   } else {
-     let weixinUrl =
-       'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe7b477d2ec9d54ec&redirect_uri=https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
-     window.location.replace(weixinUrl);
-     return;
-   }
+   window.alertDialog(href);
+   //  if (href.indexOf('code=') >= 0) {
+   //
+   //  } else {
+   //    let weixinUrl =
+   //      'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe7b477d2ec9d54ec&redirect_uri=https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
+   //    window.location.replace(weixinUrl);
+   //    return;
+   //  }
    //  let href =
    //    "https://www.maywidehb.com/webapp/index.html?code=081UhLaK1Ylsz70aUt9K1yvEaK1UhLas&state=123";
    let param = href.split("?")[1];
