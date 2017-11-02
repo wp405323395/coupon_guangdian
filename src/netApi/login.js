a66d387e59c9f0610540fa74ec509024657c2eae
max_wang <405323395@qq.com>
2017-11-02 13:07:28 +0800

    项目进度

--- src/netApi/login.js
+++ src/netApi/login.js
@@ -15,17 +15,17 @@
    //  }
    //  let href =
    //    "https://www.maywidehb.com/webapp/index.html?code=081UhLaK1Ylsz70aUt9K1yvEaK1UhLas&state=123";
-   let param = href.split("?")[1];
-   let wxcodemap = param.split("&")[0];
-   let wxcode = wxcodemap.split("=")[1];
-   new RequestEngine().request(urls.loginUrl, {
-       loginCode: wxcode
-     },
-     successValue => {
-       loginCallBack.success(successValue);
-     }, failValue => {
-       loginCallBack.faild(failValue);
-     }, completeValue => {})
+   //  let param = href.split("?")[1];
+   //  let wxcodemap = param.split("&")[0];
+   //  let wxcode = wxcodemap.split("=")[1];
+   //  new RequestEngine().request(urls.loginUrl, {
+   //      loginCode: wxcode
+   //    },
+   //    successValue => {
+   //      loginCallBack.success(successValue);
+   //    }, failValue => {
+   //      loginCallBack.faild(failValue);
+   //    }, completeValue => {})
 
  }
 
