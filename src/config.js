ace5f0fc1b8fc63acba7a77f362fd46eaf08c86d
max_wang <405323395@qq.com>
2017-11-02 16:30:53 +0800

    项目进度

--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@ var host = "www.maywidehb.com"
 var isHttps = true;
 var isDebug = true;
 var schema = isHttps ? 'https' : 'http';
+var timeOut = 10000; //设置请求超时时间
 var config = {
   // 下面的地址配合云端 Server 工作
   host,
+  timeOut,
   isDebug,
   srcUrl: `${schema}://${host}/`,
     // 登录地址，用于建立会话
