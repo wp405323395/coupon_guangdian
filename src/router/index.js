112299d9184feeed11004586f325319ebdd429f1
max_wang <405323395@qq.com>
2017-11-08 11:40:24 +0800

    项目进度

--- src/router/index.js
+++ src/router/index.js
@@ -15,19 +15,19 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      if (from.meta.keepAlive) {
-        from.meta.savedPosition = document.body.scrollTop
-      }
-      return {
-        x: 0,
-        y: to.meta.savedPosition || 0
-      }
-    }
-  },
+  // scrollBehavior(to, from, savedPosition) {
+  //   if (savedPosition) {
+  //     return savedPosition
+  //   } else {
+  //     if (from.meta.keepAlive) {
+  //       from.meta.savedPosition = document.body.scrollTop
+  //     }
+  //     return {
+  //       x: 0,
+  //       y: to.meta.savedPosition || 0
+  //     }
+  //   }
+  // },
   routes: [{
     path: '/',
     name: '',
