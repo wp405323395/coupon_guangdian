a398373a3510b2ec300de2008bbbe7c4e3c02e90
max_wang <405323395@qq.com>
2017-11-08 11:46:49 +0800

    项目进度

--- src/router/index.js
+++ src/router/index.js
@@ -14,55 +14,71 @@ import welcome from '@/page/welcome'
 Vue.use(Router)
 
 export default new Router({
-  // scrollBehavior(to, from, savedPosition) {
-  //   if (savedPosition) {
-  //     return savedPosition
-  //   } else {
-  //     if (from.meta.keepAlive) {
-  //       from.meta.savedPosition = document.body.scrollTop
-  //     }
-  //     return {
-  //       x: 0,
-  //       y: to.meta.savedPosition || 0
-  //     }
-  //   }
-  // },
-  routes: [{
-    path: '/',
-    name: '',
-    component: App,
-    children: [{
-      path: '',
-      redirect: '/welcome',
-      name: 'welcome'
-    }, {
-      path: '/welcome',
-      component: welcome,
-      name: 'welcome'
-    }, {
-      path: '/tvProgram',
-      component: tvProgram,
-      name: 'tvProgram'
-    }, {
-      path: '/index',
-      component: index,
-      name: '往期回顾'
-    }, {
-      path: '/main/',
-      component: main,
+  scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        if (from.meta.keepAlive) {
+          from.meta.savedPosition = document.body.scrollTop
+        }
+        return {
+          x: 0,
+          y: to.meta.savedPosition || 0
+        }
+      }
+    },
+    routes: [{
+      path: '/',
+      name: '',
+      component: App,
       children: [{
-        path: '/',
-        component: voteList,
-        name: 'homeMain'
+        path: '',
+        redirect: '/welcome',
+        name: 'welcome',
+        meta: {
+          title: 'tvProgram',
+          keepAlive: false
+        }
       }, {
-        path: '/main/createVote',
-        component: createVote,
-        name: 'createVote'
+        path: '/welcome',
+        component: welcome,
+        name: 'welcome',
+        meta: {
+          title: 'tvProgram',
+          keepAlive: false
+        }
       }, {
-        path: '/main/voteDetail',
-        component: voteDetail,
-        name: 'voteDetail'
+        path: '/tvProgram',
+        component: tvProgram,
+        name: 'tvProgram',
+        meta: {
+          title: 'tvProgram',
+          keepAlive: false
+        }
+      }, {
+        path: '/index',
+        component: index,
+        name: '往期回顾',
+        meta: {
+          title: 'index',
+          keepAlive: true
+        }
+      }, {
+        path: '/main/',
+        component: main,
+        children: [{
+          path: '/',
+          component: voteList,
+          name: 'homeMain'
+        }, {
+          path: '/main/createVote',
+          component: createVote,
+          name: 'createVote'
+        }, {
+          path: '/main/voteDetail',
+          component: voteDetail,
+          name: 'voteDetail'
+        }]
       }]
     }]
-  }]
 })
