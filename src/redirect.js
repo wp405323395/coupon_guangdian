c5890e6c2c17a866bfb2ba93ba159b8cdf4b9cda
max_wang <405323395@qq.com>
2017-11-10 14:44:03 +0800

    项目进度

--- src/redirect.js
+++ src/redirect.js
@@ -1,22 +1,18 @@
-window.onload = function() {
-  if (process.env.NODE_ENV === 'production') {
-    let loadcount = sessionStorage.getItem('loadNum');
-    if (loadcount == undefined || loadcount == null || loadcount == '') {
-      sessionStorage.setItem('loadNum', 1);
-    }
-
-    let href = window.location.href;
-    if (href.indexOf('welcome') >= 0) {} else {
-      let startRefreshCount = sessionStorage.getItem('loadNum');
-      if (startRefreshCount < 2) {
-        sessionStorage.setItem('loadNum', 3);
-        let weixinUrl =
-          'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe7b477d2ec9d54ec&redirect_uri=https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
-        window.location = weixinUrl;
-      }
-
-    }
+if (process.env.NODE_ENV === 'production') {
+  let loadcount = sessionStorage.getItem('loadNum');
+  if (loadcount == undefined || loadcount == null || loadcount == '') {
+    sessionStorage.setItem('loadNum', 1);
   }
 
+  let href = window.location.href;
+  if (href.indexOf('welcome') >= 0) {} else {
+    let startRefreshCount = sessionStorage.getItem('loadNum');
+    if (startRefreshCount < 2) {
+      sessionStorage.setItem('loadNum', 3);
+      let weixinUrl =
+        'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe7b477d2ec9d54ec&redirect_uri=https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
+      window.location = weixinUrl;
+    }
 
+  }
 }
