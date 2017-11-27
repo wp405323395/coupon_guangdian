2345eb26c980efacbae8abd2a9fb141617cfe460
max_wang <405323395@qq.com>
2017-11-27 09:54:16 +0800

    项目进度

--- src/redirect.js
+++ src/redirect.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
     if (startRefreshCount < 2) {
       sessionStorage.setItem('loadNum', 3);
       let weixinUrl =
-        'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe7b477d2ec9d54ec&redirect_uri=https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
+        'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5d63c4a1990efe79&redirect_uri=https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
       window.location = weixinUrl;
     }
 
