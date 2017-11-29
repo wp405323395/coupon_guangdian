a694f2b0e4b1f384b93faa21f13f51598b4272f6
max_wang <405323395@qq.com>
2017-11-29 12:01:45 +0800

    项目进度

--- src/WXJsSdk.js
+++ src/WXJsSdk.js
@@ -2,7 +2,7 @@ import RequestEngine from 'netenginwang'
   //process.env.NODE_ENV === 'production'
 if (true) {
   let urls = require("./config")
-  let wx = require('./libs/jweixin-1.2.0');
+  let wx = require('./libs/jweixin-1.2.0').wx;
   new Promise((resolve, reject) => {
     new RequestEngine().request(urls.wxJsSDK, {
         "url": location.href
@@ -13,7 +13,7 @@ if (true) {
         reject(failValue);
       }, completeValue => {})
   }).then(value => {
-    wx.wx.config({
+    wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: value.appId, // 必填，公众号的唯一标识
       timestamp: value.timestamp, // 必填，生成签名的时间戳
@@ -24,33 +24,35 @@ if (true) {
           "onMenuShareAppMessage" //分享给朋友接口
         ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
-    wx.wx.ready(function() {
+    wx.ready(function() {
+      console.log('kkk->ready');
       new RequestEngine().request(urls.wxJsSDK, {
             "state": 'ready'
           },
           successValue => {}, failValue => {}, completeValue => {})
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
     });
-    wx.wx.error(function(res) {
+    wx.error(function(res) {
+      console.log('kkk->err');
       new RequestEngine().request(urls.wxJsSDK, {
             "state": ('失败->' + res)
           },
           successValue => {}, failValue => {}, completeValue => {})
         // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
     });
-    wx.wx.checkJsApi({
+    wx.checkJsApi({
       jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
       success: function(res) {
         // 以键值对的形式返回，可用的api值true，不可用为false
         // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-        console.log(res);
+        console.log('kkk->checkApi');
         new RequestEngine().request(urls.wxJsSDK, {
             "state": res
           },
           successValue => {}, failValue => {}, completeValue => {})
       }
     });
-    wx.wx.onMenuShareAppMessage({
+    wx.onMenuShareAppMessage({
       title: '自定义分享标题', // 分享标题
       desc: '自定义分享描述', // 分享描述
       link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
@@ -69,11 +71,7 @@ if (true) {
       }
     });
   }).catch(err => {
-    new RequestEngine().request(urls.wxJsSDK, {
-          "state": ('异常' + err)
-        },
-        successValue => {}, failValue => {}, completeValue => {})
-      // 用户确认分享后执行的回调函数
+    console.log('kkk->err', err);
   });
 
 
