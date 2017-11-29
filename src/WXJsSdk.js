fc3cdf66b21286aa53399139a67edcc3aa3c6fa9
max_wang <405323395@qq.com>
2017-11-29 11:40:32 +0800

    项目进度

--- src/WXJsSdk.js
+++ src/WXJsSdk.js
@@ -1,5 +1,6 @@
 import RequestEngine from 'netenginwang'
-if (process.env.NODE_ENV === 'production') {
+  //process.env.NODE_ENV === 'production'
+if (true) {
   let urls = require("./config")
   let wx = require('./libs/jweixin-1.2.0');
   new Promise((resolve, reject) => {
@@ -12,8 +13,8 @@ if (process.env.NODE_ENV === 'production') {
         reject(failValue);
       }, completeValue => {})
   }).then(value => {
-    wx.config({
-      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    wx.wx.config({
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: value.appId, // 必填，公众号的唯一标识
       timestamp: value.timestamp, // 必填，生成签名的时间戳
       nonceStr: value.nonceStr, // 必填，生成签名的随机串
@@ -23,23 +24,21 @@ if (process.env.NODE_ENV === 'production') {
           "onMenuShareAppMessage" //分享给朋友接口
         ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
-    wx.ready(function() {
-      console.log('ready');
+    wx.wx.ready(function() {
       new RequestEngine().request(urls.wxJsSDK, {
             "state": 'ready'
           },
           successValue => {}, failValue => {}, completeValue => {})
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
     });
-    wx.error(function(res) {
-      console.log('err');
+    wx.wx.error(function(res) {
       new RequestEngine().request(urls.wxJsSDK, {
             "state": ('失败->' + res)
           },
           successValue => {}, failValue => {}, completeValue => {})
         // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
     });
-    wx.checkJsApi({
+    wx.wx.checkJsApi({
       jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
       success: function(res) {
         // 以键值对的形式返回，可用的api值true，不可用为false
@@ -51,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
           successValue => {}, failValue => {}, completeValue => {})
       }
     });
-    wx.onMenuShareAppMessage({
+    wx.wx.onMenuShareAppMessage({
       title: '自定义分享标题', // 分享标题
       desc: '自定义分享描述', // 分享描述
       link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
@@ -70,7 +69,11 @@ if (process.env.NODE_ENV === 'production') {
       }
     });
   }).catch(err => {
-
+    new RequestEngine().request(urls.wxJsSDK, {
+          "state": ('异常' + err)
+        },
+        successValue => {}, failValue => {}, completeValue => {})
+      // 用户确认分享后执行的回调函数
   });
 
 
