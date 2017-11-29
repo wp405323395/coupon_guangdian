643397c516cb38a20a8e57ddce85392d39b5fe36
max_wang <405323395@qq.com>
2017-11-29 13:53:55 +0800

    微信分享

--- src/WXJsSdk.js
+++ src/WXJsSdk.js
@@ -28,6 +28,18 @@ if (true) {
     wx.ready(function() {
       console.log('kkk->ready');
       // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+      wx.onMenuShareAppMessage({
+        title: '自定义分享标题', // 分享标题
+        desc: '自定义分享描述', // 分享描述
+        link: (window.location.href + '&kkk=1212121'), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: "https://www.maywidehb.com/webapp/dist/static/img/appicon.jpg", // 分享图标
+        type: '', // 分享类型,music、video或link，不填默认为link
+        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+        success: function() {},
+        cancel: function() {
+          // 用户取消分享后执行的回调函数
+        }
+      });
     });
     wx.error(function(res) {
       console.log('kkk->err');
