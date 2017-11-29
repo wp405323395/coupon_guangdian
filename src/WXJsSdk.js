2543dcaa9313ae059c3baa7f699a4226afc82791
max_wang <405323395@qq.com>
2017-11-29 11:19:11 +0800

    添加打印日志

--- src/WXJsSdk.js
+++ src/WXJsSdk.js
@@ -24,16 +24,31 @@ if (process.env.NODE_ENV === 'production') {
         ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
     wx.ready(function() {
-      // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+      console.log('ready');
+      new RequestEngine().request(urls.wxJsSDK, {
+            "state": 'ready'
+          },
+          successValue => {}, failValue => {}, completeValue => {})
+        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
     });
     wx.error(function(res) {
-      // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+      console.log('err');
+      new RequestEngine().request(urls.wxJsSDK, {
+            "state": ('失败->' + res)
+          },
+          successValue => {}, failValue => {}, completeValue => {})
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
     });
     wx.checkJsApi({
       jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
       success: function(res) {
         // 以键值对的形式返回，可用的api值true，不可用为false
         // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+        console.log(res);
+        new RequestEngine().request(urls.wxJsSDK, {
+            "state": res
+          },
+          successValue => {}, failValue => {}, completeValue => {})
       }
     });
     wx.onMenuShareAppMessage({
@@ -44,7 +59,11 @@ if (process.env.NODE_ENV === 'production') {
       type: '', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
       success: function() {
-        // 用户确认分享后执行的回调函数
+        new RequestEngine().request(urls.wxJsSDK, {
+              "state": '分享成功'
+            },
+            successValue => {}, failValue => {}, completeValue => {})
+          // 用户确认分享后执行的回调函数
       },
       cancel: function() {
         // 用户取消分享后执行的回调函数
