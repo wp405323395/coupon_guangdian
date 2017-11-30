d42033124d2a6fba80819f47e5f01fde433e3e79
max_wang <405323395@qq.com>
2017-11-30 16:25:28 +0800

    项目进度

--- src/redirect.js
+++ src/redirect.js
@@ -25,15 +25,15 @@ window.onload = function() {
         window.location.href = url;
       }
     } else {
-      let startRefreshCount = sessionStorage.getItem('loadNum');
-      if (startRefreshCount < 2) {
-        sessionStorage.setItem('loadNum', 3);
-        let weixinUrl =
-          'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5d63c4a1990efe79&redirect_uri=' +
-          'https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome%3froutepath%3dgame%26json%3d%257b%2522tvId%2522%253a2%257d' +
-          '&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
-        window.location = weixinUrl;
-      }
+      // let startRefreshCount = sessionStorage.getItem('loadNum');
+      // if (startRefreshCount < 2) {
+      //   sessionStorage.setItem('loadNum', 3);
+      //   let weixinUrl =
+      //     'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5d63c4a1990efe79&redirect_uri=' +
+      //     'https%3a%2f%2fwww.maywidehb.com%2fwebapp%2fdist%2f%23%2fwelcome%3froutepath%3dgame%26json%3d%257b%2522tvId%2522%253a2%257d' +
+      //     '&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
+      //   window.location = weixinUrl;
+      // }
 
     }
   }
