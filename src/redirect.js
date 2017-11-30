559e6ec9643b9a71ea1ae028cb8cfcd2b21d653a
max_wang <405323395@qq.com>
2017-11-30 17:52:46 +0800

    项目进度

--- src/redirect.js
+++ src/redirect.js
@@ -6,7 +6,7 @@ window.onload = function() {
     }
 
     let href = window.location.href;
-    alert(href);
+    //alert(href);
     if (href.indexOf('welcome') >= 0) {} else if (href.indexOf('sharByWx') >=
       0) {
       let startRefreshCount = sessionStorage.getItem('loadNum');
