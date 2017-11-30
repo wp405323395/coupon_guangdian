c21c938fa73d947769671b6a1bb5dc518b58e071
max_wang <405323395@qq.com>
2017-11-30 15:30:45 +0800

    项目进度

--- src/login.js
+++ src/login.js
@@ -2,10 +2,7 @@
  import RequestEngine from 'netenginwang'
 
  function login(loginCallBack) {
-   let href = window.location.href;
-   let param = href.split("?")[1];
-   let wxcodemap = param.split("&")[0];
-   let wxcode = wxcodemap.split("=")[1];
+   let wxcode = this.getQueryString('code');
    new RequestEngine().request(urls.loginUrl, {
        loginCode: wxcode
      },
@@ -15,6 +12,20 @@
        loginCallBack.faild(failValue);
      }, completeValue => {})
 
+ }
+
+ function getQueryString(par) {
+   var local_url = document.location.href;
+   var get = local_url.indexOf(par + "=");
+   if (get == -1) {
+     return false;
+   }
+   var get_par = local_url.slice(par.length + get + 1);
+   var nextPar = get_par.indexOf("&");
+   if (nextPar != -1) {
+     get_par = get_par.slice(0, nextPar);
+   }
+   return get_par;
  }
 
  export default login;
