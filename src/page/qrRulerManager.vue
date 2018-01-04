6e1dd726a76257d36e3e6689f69e6a7acda9af0c
max_wang <405323395@qq.com>
2018-01-04 17:04:18 +0800

    项目进度

--- src/page/qrRulerManager.vue
+++ src/page/qrRulerManager.vue
@@ -20,40 +20,40 @@
     justify-content: center;
     align-items: center;
     background-color: rgba(0, 0, 0, .5);
-    .pass_pannel{
-      width: 400px;
-      height: 200px;
-      background-color: white;
-      position: relative;
-      .title{
-        font-size: 20px;
-        align-self: center;
-        width: 100%;
-        display: block;
-        text-align: center;
-        margin-top: 55px;
-      }
-      .btn_content{
-        position: absolute;
-        width: 100%;
-        bottom: 10px;
-        display: flex;
-        button{
-          cursor: pointer;
-          height: 40px;
-          width: 100%;
-          margin-left: 10px;
-          margin-right: 10px;
-          background-color: #eeeeee;
-          color: #999999;
-          font-size: 20px;
+    .pass_pannel {
+        width: 400px;
+        height: 200px;
+        background-color: white;
+        position: relative;
+        .title {
+            font-size: 20px;
+            align-self: center;
+            width: 100%;
+            display: block;
+            text-align: center;
+            margin-top: 55px;
         }
-        button:last-child{
-          margin-left: 0;
-          background-color: #4a5cc8;
-          color: white;
+        .btn_content {
+            position: absolute;
+            width: 100%;
+            bottom: 10px;
+            display: flex;
+            button {
+                cursor: pointer;
+                height: 40px;
+                width: 100%;
+                margin-left: 10px;
+                margin-right: 10px;
+                background-color: #eeeeee;
+                color: #999999;
+                font-size: 20px;
+            }
+            button:last-child {
+                margin-left: 0;
+                background-color: #4a5cc8;
+                color: white;
+            }
         }
-      }
     }
     .reject_pannel {
         width: 400px;
@@ -432,13 +432,13 @@
         </div>
     </section>
     <section class="reject_section" v-show="isShowPassPannel">
-      <div class="pass_pannel">
-          <span class="title">{{toastText}}</span></br>
-          <div class="btn_content">
-            <button @click="isShowPassPannel=false;" type="button" name="button">取消</button>
-            <button @click="sureButton" type="button" name="button">确认</button>
-          </div>
-      </div>
+        <div class="pass_pannel">
+            <span class="title">{{toastText}}</span></br>
+            <div class="btn_content">
+                <button @click="isShowPassPannel=false;" type="button" name="button">取消</button>
+                <button @click="sureButton" type="button" name="button">确认</button>
+            </div>
+        </div>
     </section>
 </section>
 
@@ -455,8 +455,8 @@ import qrRulerManager from '../components/viewQrRulerDetail'
 export default {
     data() {
             return {
-              toastText:'确认要删除已选中规则?',
-              isShowPassPannel:false,
+                toastText: '确认要删除已选中规则?',
+                isShowPassPannel: false,
                 rejectText: '',
                 isShowRejectPannel: false,
                 ruleSelectes: [],
@@ -557,17 +557,23 @@ export default {
         },
 
         methods: {
-          sureButton(){
-            this.isShowPassPannel = false;
-            this.opraQrRuler(this.optQrRuler.qrRuler, this.optQrRuler.index, this.optQrRuler.status);
-          },
-            submitReject() {
+            sureButton() {
+                    this.isShowPassPannel = false;
+                    if(this.opAllType == '' || this.opAllType == undefined) {
+                      this.opraQrRuler(this.optQrRuler.qrRuler, this.optQrRuler.index, this.optQrRuler.status);
+                    } else {
+                      this.opraAllQrRuler(this.opAllType);
+                      this.opAllType = "";
+                    }
+
+                },
+                submitReject() {
                     this.isShowRejectPannel = false;
                     if (this.isBatch) {
                         this.opraAllQrRuler('3');
                         this.isBatch = false;
                     } else {
-                        this.opraQrRuler(this.qrRulerDeleteObj.qrRuler, this.qrRulerDeleteObj.index, this.qrRulerDeleteObj.status);
+                        this.opraQrRuler(this.optQrRuler.qrRuler, this.optQrRuler.index, this.optQrRuler.status);
                     }
                 },
                 isCanModify: function(qrRuler) {
@@ -584,7 +590,7 @@ export default {
 
                 },
                 opraQrRuler(qrRuler, index, status) {
-                    let rejectT ='';
+                    let rejectT = '';
                     if (status == '3') {
                         rejectT = this.rejectText;
                     }
@@ -592,18 +598,17 @@ export default {
                     new requestEngine().request(urls.updateQRrulesStatus, {
                             ruleids: qrRuler.id,
                             status: status,
-                            rejectText:rejectT
+                            rejectText: rejectT
                         },
                         successValue => {
                             this.qrRulers.splice(index, 1);
                         }, failValue => {}, completeValue => {})
                 },
                 opraAllQrRuler(status) {
+                  this.isShowPassPannel = false;
+                  this.isShowRejectPannel = false;
                     let proQrRulers = [];
                     let selectedQrRulerids = '';
-                    if (this.qrRulers.length == 0) {
-                        return;
-                    }
                     for (var i = 0; i < this.qrRulers.length; i++) {
                         if (i == 0) {
                             proQrRulers.push({
@@ -618,14 +623,17 @@ export default {
                             proQrRulers.push(this.qrRulers[i]);
                         }
                     }
-                    let rejectT ="";
+                    let rejectT = "";
                     if (status == '3') {
                         rejectT = this.rejectText;
+                    }
+                    if(selectedQrRulerids.length == 0) {
+                      return;
                     }
                     new requestEngine().request(urls.updateQRrulesStatus, {
                             ruleids: selectedQrRulerids,
                             status: status,
-                            rejectText:rejectT
+                            rejectText: rejectT
                         },
                         successValue => {
                             this.qrRulers = proQrRulers;
@@ -636,61 +644,67 @@ export default {
                     this.isShowPassPannel = true;
                     this.toastText = "确认审核通过已选中规则?"
                     this.optQrRuler = {
-                      qrRuler:qrRuler,
-                      index:index,
-                      status:'2'
+                        qrRuler: qrRuler,
+                        index: index,
+                        status: '2'
                     }
                 },
                 rejectQrRuler(qrRuler, index) {
-                  this.isShowPassPannel = true;
-                  this.toastText = "确认驳回已选中规则?"
-                  this.optQrRuler = {
-                    qrRuler:qrRuler,
-                    index:index,
-                    status:'3'
-                  }
+                    this.isShowRejectPannel = true;
+                    this.optQrRuler = {
+                        qrRuler: qrRuler,
+                        index: index,
+                        status: '3'
+                    }
                 },
                 publishQrRuler(qrRuler, index) {
-                  this.isShowPassPannel = true;
-                  this.toastText = "确认发布已选中规则?"
-                  this.optQrRuler = {
-                    qrRuler:qrRuler,
-                    index:index,
-                    status:'4'
-                  }
+                    this.isShowPassPannel = true;
+                    this.toastText = "确认发布已选中规则?"
+                    this.optQrRuler = {
+                        qrRuler: qrRuler,
+                        index: index,
+                        status: '4'
+                    }
                 },
                 underlineQrRuler(qrRuler, index) {
-                  this.isShowPassPannel = true;
-                  this.toastText = "确认下线已选中规则?"
-                  this.optQrRuler = {
-                    qrRuler:qrRuler,
-                    index:index,
-                    status:'5'
-                  }
+                    this.isShowPassPannel = true;
+                    this.toastText = "确认下线已选中规则?"
+                    this.optQrRuler = {
+                        qrRuler: qrRuler,
+                        index: index,
+                        status: '5'
+                    }
                 },
                 deleteQrRuler(qrRuler, index) {
                     if (this.isCanDelete(qrRuler)) {
                         this.isShowPassPannel = true;
                         this.toastText = "确认删除已选中规则?"
                         this.optQrRuler = {
-                          qrRuler:qrRuler,
-                          index:index,
-                          status:'6'
+                            qrRuler: qrRuler,
+                            index: index,
+                            status: '6'
                         }
                     }
                 },
                 passAllQrRuler() {
-                    this.opraAllQrRuler('2');
+                  this.opAllType = '2';
+                  this.isShowPassPannel = true;
+                  this.toastText = "确认审核通过已选中规则?"
                 },
                 rejectAllQrRuler() {
-                    this.isShowRejectPannel = true;
                     this.isBatch = true;
+                    this.isShowRejectPannel = true;
                 },
                 publishAllQrRuler() {
-                    this.opraAllQrRuler('4');
+                    this.opAllType = '4';
+                    this.isShowPassPannel = true;
+                    this.toastText = "确认发布已选中规则?"
+
                 },
                 underAllQrRuler() {
-                    this.opraAllQrRuler('5');
+                  this.opAllType = '5';
+                  this.isShowPassPannel = true;
+                  this.toastText = "确认下线已选中规则?"
                 },
 
                 gotoQrRulerDetail(id) {
