7a40cff0b179452a16986a53ca8c495020e6f0a1
max_wang <405323395@qq.com>
2018-07-25 11:14:57 +0800

    添加路由判断token是否为空

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
-    <span>kdkdkdk</span>
+    <el-form label-position="right" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="账号">
+        <el-input v-model="formLabelAlign.account"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.password"></el-input>
+      </el-form-item>
+      <el-button @click="login" type="primary">登陆</el-button>
+    </el-form>
   </div>
 </template>
 
 <script>
+import {Button, Form, Input, FormItem} from 'element-ui'
 export default {
   data () {
     return {
-      key: 'a'
+      key: 'a',
+      formLabelAlign: {
+        account: '',
+        password: ''
+      }
+    }
+  },
+  components: {
+    'el-form': Form,
+    'el-form-item': FormItem,
+    'el-input': Input,
+    'el-button': Button
+  },
+  methods: {
+    login () {
+      sessionStorage.setItem('token', {account: this.account, password: this.password})
+      this.$router.replace({path: '/HelloWorld'})
     }
   }
 }
 </script>
 
 <style scoped>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,20 +5,46 @@ import demon from '@/components/demon'
 import login from '@/page/login'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: login
+    }, {
+      path: '/login',
+      component: login
+    }, {
+      path: '/HelloWorld',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: {
+        requireAuth: true
+      }
     }, {
       path: '/demon',
       name: 'demon',
-      component: demon
-    }, {
-      path: '/login',
-      name: 'login',
-      component: login
+      component: demon,
+      meta: {
+        requireAuth: true
+      }
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  let token = sessionStorage.getItem('token')
+  if (to.meta.requireAuth) {
+    if (token) {
+      next()
+    } else {
+      console.log('路由进入时，判断没有token，跳入登陆界面')
+      next({
+        path: '/login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  } else {
+    next()
+  }
+})
+export default router
